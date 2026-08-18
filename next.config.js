86acb44569692e84ca28281e5338ee96753572a0
Nawajish <mdnawajish.a@2coms.com>
2026-08-18 19:16:04 +0530

    Configure server-level redirect from / to /dashboard

--- next.config.js
+++ next.config.js
@@ -2,5 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   eslint: { ignoreDuringBuilds: true },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/dashboard",
+        permanent: false,
+      },
+    ];
+  },
 };
 module.exports = nextConfig;
